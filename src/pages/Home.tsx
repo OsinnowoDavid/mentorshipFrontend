4174b3e2ed7f5a9a3e5bbf572f27b6af7bfd88ca
Osinnowo David <davidadewale151@gmail.com>
2025-07-08 19:50:12 +0100

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,7 +18,7 @@ function Home() {
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
           <div className='flex justify-between items-center py-4'>
             <h1 className='text-white font-bold text-2xl'>
-              Mentorship Platform
+              Mentify
             </h1>
             <div className='flex space-x-4'>
               {isAuthenticated ? (
